dbfe07455607fd74b84160486c5cc4201377e795
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-09 14:51:27 +0900

    adapt rack-cors

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 # NameError: uninitialized constant Mail::TestMailer対策
 gem 'net-smtp'
-
+gem 'rack-cors'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
     # protect_from_forgery with: :null_session
-    protect_from_forgery with: :exception
+    # protect_from_forgery :except => :complete
+    skip_before_action :verify_authenticity_token
 end
