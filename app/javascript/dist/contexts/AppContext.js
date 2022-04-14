2e66b4eaf938d9360a0715119e28b3412f36586e
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-14 13:18:37 +0900

    From javascript to typescript

--- app/javascript/dist/contexts/AppContext.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-// const AppContext = createContext<Context | undefined>(undefined);
-const AppContext = (0, react_1.createContext)({});
-exports.default = AppContext;
