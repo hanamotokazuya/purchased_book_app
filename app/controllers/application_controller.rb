e903b377805891734748384bb40cd0632765f22e
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-14 21:32:09 +0900

    debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     include SessionsHelper
     # protect_from_forgery with: :null_session
     # protect_from_forgery :except => :complete
-    protect_from_forgery
-    # skip_before_action :verify_authenticity_token
+    # protect_from_forgery
+    skip_before_action :verify_authenticity_token
 end
