2cf8356df7dbce2b6bc9a809dc7d3ac189871700
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-14 20:46:39 +0900

    modify cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,11 +11,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     #本番環境用のオリジン設定
     allow do
-        origins 'https:<自身が設定するアプリのURL>'
+        origins 'https://purchased-book-app.herokuapp.com'
 
         resource '*',
             headers: :any,
             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            expose: ["X-CSRF_Token"],
             credentials: true
     end
 end
