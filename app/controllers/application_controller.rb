aefcf1a47be762ab527de5784112a6697a562083
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-15 17:03:51 +0900

    modify application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
     # protect_from_forgery with: :null_session
-    protect_from_forgery :except => :complete
+    # protect_from_forgery :except => :complete
     # protect_from_forgery
-    # skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 end
