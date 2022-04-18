a9c187bc0a8033be27261b0afd746aa0fcdab078
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-18 21:55:53 +0900

    refactoring Header

--- app/javascript/components/CreateBook.tsx
+++ app/javascript/components/CreateBook.tsx
@@ -1,92 +1,23 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext} from "react";
 import AppContext from "../contexts/AppContext";
 import styled from "styled-components";
-import axios, { AxiosResponse } from "axios";
-import { toast } from "react-toastify";
-import { CATEGORIES } from "../constants";
-import { useNavigate } from "react-router-dom";
-import { CgClose } from "react-icons/cg";
+import axios, {AxiosResponse} from "axios";
+import {toast} from "react-toastify";
+import {CATEGORIES} from "../constants";
+import {useNavigate} from "react-router-dom";
+import {CgClose} from "react-icons/cg";
 
 axios.defaults.headers.common = {
   "X-Requested-With": "XMLHttpRequest",
   "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content"),
 };
 
-const Base = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-const Wrapper = styled.div`
-  width: 280px;
-  height: 200px;
-  padding: 5px;
-  border-radius: 4px;
-  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-const Icon = styled.div`
-  width: 250px;
-  display: flex;
-  justify-content: right;
-  margin-bottom: 5px;
-`;
-const CreateBookForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-const CategorySelect = styled.select`
-  font-size: 14px;
-  width: 250px;
-  height: 30px;
-  margin-bottom: 10px;
-`;
-const TitleInput = styled.input`
-  font-size: 14px;
-  width: 250px;
-  height: 30px;
-  margin-bottom: 10px;
-`;
-const FileField = styled.input`
-  font-size: 14px;
-  width: 250px;
-  height: 30px;
-  margin-bottom: 10px;
-`;
-const SubmitButton = styled.button`
-  font-size: 14px;
-  width: 250px;
-  height: 30px;
-  border-radius: 5px;
-  background-color: white;
-  color: #000000;
-`;
-const ErrorMessageBox = styled.div`
-  width: 300px;
-  padding: 10px;
-  margin-top: 10px;
-  color: red;
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
-`;
-const ErrorMessage = styled.p`
-  font-size: 14px;
-`;
 
 type Props = {
   close: () => void;
-  // close: React.MouseEventHandler;
 };
-function CreateBook({ close }: Props) {
-  const { dispatch } = useContext(AppContext);
+function CreateBook({close}: Props) {
+  const {dispatch} = useContext(AppContext);
   const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
@@ -108,11 +39,11 @@ function CreateBook({ close }: Props) {
   };
   const handleClickSubmitBook = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const data = { title, category, image };
+    const data = {title, category, image};
     axios
-      .post("/api/v1/books/create", { book: data })
+      .post("/api/v1/books/create", {book: data})
       .then((res: AxiosResponse<Book | Array<string>>) => {
-        const { data } = res;
+        const {data} = res;
         if (!Array.isArray(data)) {
           dispatch({
             type: "CREATE_BOOK_EVENT",
@@ -121,7 +52,7 @@ function CreateBook({ close }: Props) {
           toast.success("新しく本が追加されました！");
           setTitle("");
           setCategory("");
-          setImage({ data: "", name: "" });
+          setImage({data: "", name: ""});
           close();
           navigate("/books");
         } else {
@@ -134,36 +65,104 @@ function CreateBook({ close }: Props) {
   };
   return (
     <Base>
-      <Wrapper>
-        <Icon>
-          <CgClose style={{ cursor: "pointer" }} onClick={() => close()} />
-        </Icon>
-        <CreateBookForm>
-          <TitleInput
-            type="text"
-            value={title}
-            placeholder="本のタイトルを入力してください"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <CategorySelect value={category} onChange={(e) => setCategory(e.target.value)}>
-            <option hidden>選択してください</option>
-            {CATEGORIES.map((category, key) => (
-              <option key={key}>{category}</option>
+      <BoxWrapper>
+        <FormBox>
+          <div className="icon">
+            <CgClose style={{cursor: "pointer"}} onClick={() => close()} />
+          </div>
+          <form className="create-book-form">
+            <input
+              className="form-item"
+              type="text"
+              value={title}
+              placeholder="本のタイトルを入力してください"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <select className="form-item" value={category} onChange={(e) => setCategory(e.target.value)}>
+              <option hidden>選択してください</option>
+              {CATEGORIES.map((category, key) => (
+                <option key={key}>{category}</option>
+              ))}
+            </select>
+            <input className="form-item" type="file" accept="image/*,.png,.jpg,.jpeg,.gif" onChange={getImage} />
+            <button className="submit" onClick={handleClickSubmitBook}>追加</button>
+          </form>
+        </FormBox>
+        {!!errors.length && (
+          <ErrorMessageBox>
+            {errors.map((error, i) => (
+              <p key={i}>{error}</p>
             ))}
-          </CategorySelect>
-          <FileField type="file" accept="image/*,.png,.jpg,.jpeg,.gif" onChange={getImage} />
-          <SubmitButton onClick={handleClickSubmitBook}>追加</SubmitButton>
-        </CreateBookForm>
-      </Wrapper>
-      {!!errors.length && (
-        <ErrorMessageBox>
-          {errors.map((error, i) => (
-            <ErrorMessage key={i}>{error}</ErrorMessage>
-          ))}
-        </ErrorMessageBox>
-      )}
+          </ErrorMessageBox>
+        )}
+      </BoxWrapper>
     </Base>
   );
 }
 
 export default CreateBook;
+
+const Base = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  opacity: 1;
+  background: rgba(0,0,0,0.6);
+  z-index: 9999;
+`;
+const BoxWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const FormBox = styled.div`
+  width: 280px;
+  height: 200px;
+  padding: 5px;
+  border-radius: 4px;
+  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .icon {
+    width: 250px;
+    display: flex;
+    justify-content: right;
+    margin-bottom: 5px;
+  }
+  .create-book-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .form-item {
+    width: 250px;
+    height: 30px;
+    margin-bottom: 10px;
+  }
+  .submit {
+    width: 250px;
+    height: 30px;
+    border-radius: 5px;
+    background-color: white;
+    color: #000000;
+  }
+`;
+const ErrorMessageBox = styled.div`
+  width: 280px;
+  padding: 10px;
+  margin-top: 10px;
+  color: red;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
+`;
--- app/javascript/components/Header.tsx
+++ app/javascript/components/Header.tsx
@@ -1,50 +1,149 @@
-import React, { useContext, useState } from "react";
-import { Routes, Route, Link as Li, Outlet } from "react-router-dom";
-import { useSpring, animated } from "react-spring";
+import React, {useContext, useState} from "react";
+import {Routes, Route, Link, Outlet} from "react-router-dom";
+import {useSpring, animated} from "react-spring";
 import styled from "styled-components";
 import axios from "axios";
-import { pc, tab, sp } from "../utils/media";
+import {pc, tab, sp} from "../utils/media";
 import AppContext from "../contexts/AppContext";
 import useInterval from "../utils/useInterval";
 import Hamburger from "./Hamburger";
 import CreateBook from "./CreateBook";
 import SearchBar from "./SearchBar";
+import {CSSTransition} from "react-transition-group";
 
 axios.defaults.headers.common = {
   "X-Requested-With": "XMLHttpRequest",
   "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content"),
 };
 
+
+function Header() {
+  const {
+    state: {showBooks, isSignIn},
+    dispatch,
+  } = useContext(AppContext);
+  const [resultBookCount, setResultBookCount] = useState(0);
+  const [isOpenAddPage, setIsOpenAddPage] = useState(false);
+  const spring = useSpring({
+    opacity: isOpenAddPage ? "1" : "0",
+    display: isOpenAddPage ? "block" : "none",
+    config: {duration: 250},
+  });
+  function countUpDown() {
+    resultBookCount < showBooks.length && setResultBookCount(resultBookCount + 1);
+    resultBookCount > showBooks.length && setResultBookCount(resultBookCount - 1);
+  }
+
+  const signout = () => {
+    // sessionとcookiesを掃除する処理を書く
+    axios.delete("/api/v1/sessions/destroy");
+    // カレントユーザーのステートを初期化する
+    dispatch({type: "SIGN_OUT_EVENT"});
+  };
+  useInterval(countUpDown, 10);
+
+  return (
+    <Base>
+      <div className="wrapper">
+        <LeftSide>
+          <Logo>書籍<br />管理</Logo>
+          {isSignIn && (
+            <>
+              <CSSTransition in={isOpenAddPage} timeout={700} classNames="fade" unmountOnExit>
+                <CreateBook close={() => setIsOpenAddPage(!isOpenAddPage)} />
+              </CSSTransition>
+              <Routes>
+                <Route path="/books" element={<SearchBar />} />
+                <Route path="*" element={<Outlet />} />
+              </Routes>
+              <Routes>
+                <Route path="/books" element={<p className="result-book-count">{resultBookCount}</p>} />
+                <Route path="*" element={<Blank />} />
+              </Routes>
+            </>
+          )}
+        </LeftSide>
+        {isSignIn && (
+          <>
+            <Nav>
+              <Link className="link" to="/books">本を並べる</Link>
+              <Link className="link" to="/pie_chart">パイチャート</Link>
+              <button className="add-book-btn" onClick={() => setIsOpenAddPage(!isOpenAddPage)}>本を追加する</button>
+              <Link className="link" to="/signin" onClick={signout}>サインアウト</Link>
+            </Nav>
+            <Hamburger signout={signout} addBook={() => setIsOpenAddPage(!isOpenAddPage)} />
+          </>
+        )}
+      </div>
+    </Base>
+  );
+}
+
+export default Header;
+
 const Base = styled.header`
   position: fixed;
   z-index: 10;
   background-color: #111111;
   width: 100%;
-`;
-const Wrapper = styled.div`
-  margin: 0 auto;
-  padding: 10px;
-  max-width: 1024px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  .wrapper {
+    margin: 0 auto;
+    padding: 10px;
+    max-width: 1024px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 const LeftSide = styled.div`
   display: flex;
   justify-content: left;
   align-items: center;
-  ${pc`
   height: 72px;
-  `}
   ${tab`
-  height: 56px;
+    height: 56px;
   `}
   ${sp`
-  height: 56px;
+    height: 56px;
   `}
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    opacity: 1;
+  transition: opacity 0.3s;
+  }
+  .fade-exit {
+    opacity: 1;
+  }
+  .fade-exit-active {
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  .result-book-count {
+    color: #eaeded;
+    font-weight: bold;
+    font-size: 48px;
+    &::after {
+      content: "冊";
+      font-size: 16px;
+    }
+    ${tab`
+      font-size: 32px;
+      &::after {
+        font-size: 16px;
+      }
+    `}
+    ${sp`
+      font-size: 16px;
+      &::after {
+        font-size: 8px;
+      }
+    `}
+  }
 `;
 
-const Logo = styled.div`
+const Logo = styled.h1`
   border-color: #eaeded;
   border-radius: 35%;
   display: flex;
@@ -72,152 +171,39 @@ const Logo = styled.div`
   `}
 `;
 
-const ResultBookCount = styled.p`
-  color: #eaeded;
-  font-weight: bold;
-  ${pc`
-    font-size: 48px;
-    &::after {
-      content: "冊";
-      font-size: 16px;
-    }
-  `}
-  ${tab`
-    font-size: 32px;
-    &::after {
-      content: "冊";
-      font-size: 16px;
-    }
-  `}
-  ${sp`
-    font-size: 16px;
-    &::after {
-      content: "冊";
-      font-size: 8px;
-    }
-  `}
-`;
 const Blank = styled.div`
   margin: 0 auto;
 `;
-const Links = styled.div`
+const Nav = styled.nav`
   display: flex;
   justify-content: right;
-  ${pc`
-    display: block;
-  `}
+  display: block;
   ${tab`
     display: none;
   `}
   ${sp`
     display: none;
   `}
-`;
-
-const AddBookButton = styled.button`
-  color: white;
-  font-size: 14px;
-  border: none;
-  padding: 3px 10px;
-  background-color: transparent;
-  cursor: pointer;
-  &:hover {
-    background-color: #eb6100;
+  .link {
+    font-size: 14px;
+    padding: 3px 10px;
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+    &:hover {
+      background-color: #eb6100;
+    }
   }
-`;
-const UpDownAnime = styled(animated.div)`
-  position: fixed;
-  width: 100%;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.6);
-  z-index: 9999;
-`;
-const Link = styled(Li)`
-  font-size: 14px;
-  padding: 3px 10px;
-  color: white;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #eb6100;
-  }
-`;
-
-function Header() {
-  const {
-    state: { showBooks, isSignIn },
-    dispatch,
-  } = useContext(AppContext);
-  const [resultBookCount, setResultBookCount] = useState(0);
-  const [isOpenAddPage, setIsOpenAddPage] = useState(false);
-  const spring = useSpring({
-    opacity: isOpenAddPage ? "1" : "0",
-    display: isOpenAddPage ? "block" : "none",
-    config: { duration: 250 },
-  });
-  function countUpDown() {
-    resultBookCount < showBooks.length && setResultBookCount(resultBookCount + 1);
-    resultBookCount > showBooks.length && setResultBookCount(resultBookCount - 1);
-  }
-
-  const signout = () => {
-    // sessionとcookiesを掃除する処理を書く
-    axios.delete("/api/v1/sessions/destroy");
-    // カレントユーザーのステートを初期化する
-    dispatch({ type: "SIGN_OUT_EVENT" });
-  };
-  useInterval(countUpDown, 10);
-
-  return (
-    <Base>
-      <Wrapper>
-        <LeftSide>
-          <Logo>
-            <p>
-              書籍
-              <br />
-              管理
-            </p>
-          </Logo>
-          {isSignIn && (
-            <>
-              <UpDownAnime style={spring}>
-                <CreateBook close={() => setIsOpenAddPage(!isOpenAddPage)} />
-              </UpDownAnime>
-              <Routes>
-                <Route path="/books" element={<SearchBar />} />
-                <Route path="*" element={<Outlet />} />
-              </Routes>
-              <Routes>
-                <Route
-                  path="/books"
-                  element={<ResultBookCount>{resultBookCount}</ResultBookCount>}
-                />
-                <Route path="*" element={<Blank />} />
-              </Routes>
-            </>
-          )}
-        </LeftSide>
-        {isSignIn && (
-          <>
-            <Links>
-              <Link to="/books">本を並べる</Link>
-              <Link to="/pie_chart">パイチャート</Link>
-              <AddBookButton onClick={() => setIsOpenAddPage(!isOpenAddPage)}>
-                本を追加する
-              </AddBookButton>
-              <Link to="/signin" onClick={signout}>
-                サインアウト
-              </Link>
-            </Links>
-            <Hamburger signout={signout} addBook={() => setIsOpenAddPage(!isOpenAddPage)} />
-          </>
-        )}
-      </Wrapper>
-    </Base>
-  );
+  .add-book-btn {
+    color: white;
+    font-size: 14px;
+    border: none;
+    padding: 3px 10px;
+    background-color: transparent;
+    cursor: pointer;
+    &:hover {
+      background-color: #eb6100;
+    }
 }
+`;
 
-export default Header;
